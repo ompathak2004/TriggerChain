e7c2daea9aad79df5b795831f5435ec26b6020ad
ompathak2004 <om2004.pathak@gmail.com>
2024-08-18 15:02:12 +0530

    updated gitignore

--- worker/dist/index.js
+++ worker/dist/index.js
@@ -1,1 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const kafkajs_1 = require("kafkajs");
+const TOPIC_NAME = "zap-events";
+const kafka = new kafkajs_1.Kafka({
+    clientId: "outbox-processor",
+    brokers: ["localhost:9092"],
+});
